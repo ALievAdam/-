2e4e8c7c58677348edf87524d2b5c871101efe24
ALievAdam <90550031+ALievAdam@users.noreply.github.com>
2021-12-23 20:01:55 +0500

    написал игру

---
+++ CoolGame-master/CoolGame/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoolGame
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, jump, gameover;
+        int force;
+        int gravity = 2;
+        int jumpForce = 20;
+        int horSpeed = 40;
+        int verSpeed = 4;
+        int playerSpeed = 25;
+        int score = 30;
+
+        public Form1()
+        {
+            Instructions i = new Instructions();
+            i.ShowDialog();
+            InitializeComponent();
+            this.SetStyle(ControlStyles.UserPaint |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.DoubleBuffer, true);
+            KeyPreview = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exitImage_MouseHover(object sender, EventArgs e)
+        {
+            exitImage.Image = Properties.Resources.exitImageMouse;
+        }
+
+        private void exitImage_MouseLeave(object sender, EventArgs e)
+        {
+            exitImage.Image = Properties.Resources.exitImage;
+        }
+    }
+}
---
+++ CoolGame-master/CoolGame/PassLvl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoolGame
+{
+    public partial class PassLvl : Form
+    {
+        private Form1 form2;
+        public PassLvl(Form1 form2)
+        {
+            InitializeComponent();
+            this.form2 = form2;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();
+            form2.Close();
+        }
+    }
+}
